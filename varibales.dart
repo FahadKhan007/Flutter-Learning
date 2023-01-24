633ed1af4093bdf0d075e75eb582f28532239b3d
Abdullah Al Fahad <52467937+FahadKhan007@users.noreply.github.com>
2023-01-24 10:37:38 +0600

    Create varibales.dart

---
+++ varibales.dart
@@ -0,0 +1,15 @@
+// Variables 
+
+// Final variables can not be chaged
+
+const age =  24;
+const twiceTheAge = age * 2;
+void main() {
+  var name = 'Alex';
+  name = 'Antony';
+  
+  final name2 = 'Fahad';
+  //name2 = 'Khan'; --> Can not be assigned
+  print(name);
+  print(name2);
+}
